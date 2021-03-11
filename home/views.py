a12fee97bd806d52ef238dc299cec856d0f8a5bc
nualagr <nualagreenwood@gmail.com>
2021-03-11 16:33:41 +0000

    feat: Add our_story.html, views, url and content.

--- home/views.py
+++ home/views.py
@@ -7,3 +7,8 @@ def index(request):
     """ A view to return the index page. """
 
     return render(request, 'home/index.html')
+
+
+def our_story(request):
+    """ A view to render Our Story page. """
+    return render(request, 'home/our_story.html')
