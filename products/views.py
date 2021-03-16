42bfb2829bf467dfc989577751e6d4eec81b38c8
nualagr <nualagreenwood@gmail.com>
2021-03-16 15:18:32 +0000

    feat: Add pagination and pagination navigation buttons to products.html page.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Product
 
@@ -9,9 +10,16 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+    paginator = Paginator(products, 5)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    page_range = paginator.page_range
 
     context = {
             "products": products,
+            "page_obj": page_obj,
+            "page_range": page_range,
         }
 
     return render(request, "products/products.html", context)
