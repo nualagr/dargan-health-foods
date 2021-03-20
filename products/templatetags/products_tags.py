7041a64ff136838ec162c9d2c3be8d578df8df84
nualagr <nualagreenwood@gmail.com>
2021-03-20 10:55:13 +0000

    fix: Remove section-container and section-title-container into their own divs and out of the row divs in order to get rid of the conflicting padding/margins which were causing overflow on the index.html page.

--- products/templatetags/products_tags.py
+++ products/templatetags/products_tags.py
@@ -3,6 +3,8 @@ from django import template
 register = template.Library()
 
 
+# Split Filter Tag found at:
+# https://roytuts.com/creating-custom-template-tags-and-filter-in-django/
 @register.filter(name='split')
 def split(str, delimiter):
     return str.split(delimiter)
