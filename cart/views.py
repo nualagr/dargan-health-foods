bdc65f59c6574813bb2e929e076c897a35a1249c
nualagr <nualagreenwood@gmail.com>
2021-03-26 11:20:28 +0000

    Add Shopping Cart app, urls and basic template.

---
+++ cart/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def view_cart(request):
+    """
+    A view that renders the shopping cart contents page.
+    """
+    return render(request, "cart/cart.html")
