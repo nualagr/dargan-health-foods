86a59315d4801f91363b1d7d809a2da4b5cfde02
nualagr <nualagreenwood@gmail.com>
2021-03-27 16:35:15 +0000

    style: Style Cart template and add the 'for' loops and 'if' statements necessary to check whether the information to be displayed is in the context.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
@@ -7,6 +9,18 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    # Get the cart if it exists or initialize it to an empty dict if not
+    cart = request.session.get("cart", {})
+    # Iterate through all the items in the cart
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            "item_id": item_id,
+            "quantity": quantity,
+            "product": product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -18,13 +32,13 @@ def cart_contents(request):
     grand_total = delivery + total
 
     context = {
-        'cart_items': cart_items,
-        'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total,
+        "cart_items": cart_items,
+        "total": total,
+        "product_count": product_count,
+        "delivery": delivery,
+        "free_delivery_delta": free_delivery_delta,
+        "free_delivery_threshold": settings.FREE_DELIVERY_THRESHOLD,
+        "grand_total": grand_total,
     }
 
     return context
