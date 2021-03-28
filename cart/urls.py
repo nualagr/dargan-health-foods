7b9153de9ceea8f34df8354fcc8a0d9bd202c59b
nualagr <nualagreenwood@gmail.com>
2021-03-28 15:59:03 +0000

    feat: Add increment and decrement buttons and their respective views to the quantity column of each item in the shopping cart to facilitate users changing the quantity of the products they wish to buy.

--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_cart, name='view_cart'),
-    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path("", views.view_cart, name="view_cart"),
+    path("add/<item_id>/", views.add_to_cart, name="add_to_cart"),
+    path(
+        "increase_quantity/<item_id>/",
+        views.increase_quantity_by_one,
+        name="increase_quantity_by_one",
+    ),
+    path(
+        "decrease_quantity/<item_id>/",
+        views.decrease_quantity_by_one,
+        name="decrease_quantity_by_one",
+    ),
+    path("remove/<item_id>/", views.remove_from_cart, name="remove_from_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -22,7 +22,7 @@ def add_to_cart(request, item_id):
     cart = request.session.get("cart", {})
 
     # If product id already exists in the cart
-    # Increment the quantity
+    # Increment the quantity by the requested amount
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
     else:
@@ -30,5 +30,48 @@ def add_to_cart(request, item_id):
 
     # Put the cart variable in the session
     request.session["cart"] = cart
-    print(request.session["cart"])
     return redirect(redirect_url)
+
+
+def remove_from_cart(request, item_id):
+    """
+    View to remove the specified item from the shopping cart.
+    """
+    cart = request.session.get("cart", {})
+
+    if item_id in list(cart.keys()):
+        cart.pop(item_id)
+
+    request.session["cart"] = cart
+    print(request.session["cart"])
+    return redirect(reverse('view_cart'))
+
+
+def increase_quantity_by_one(request, item_id):
+    """
+    View to increase the quantity of the
+    specified item in the shopping cart by one.
+    """
+    cart = request.session.get('cart', {})
+    if item_id in list(cart.keys()):
+        cart[item_id] += 1
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def decrease_quantity_by_one(request, item_id):
+    """
+    View to decrease the quantity of the
+    specified item in the shopping cart by one.
+    """
+
+    cart = request.session.get('cart', {})
+    if item_id in list(cart.keys()):
+        if cart[item_id] > 1:
+            cart[item_id] -= 1
+        else:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
