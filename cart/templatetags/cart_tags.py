c2383424f84636c1f6f139b1255a9677da2e42be
nualagr <nualagreenwood@gmail.com>
2021-03-29 08:17:24 +0000

    feat: Add a custom template-filter, calc_subtotal, to calculate the subtotal for each item in the shopping cart.

---
+++ cart/templatetags/cart_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+# Create a variable which is an instance
+# of the template Library
+register = template.Library()
+
+
+# Use the register filter decorator to register
+# our function as a template filter
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    return price * quantity
