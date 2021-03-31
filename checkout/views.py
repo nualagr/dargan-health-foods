724677867c3aa72ce6288c755adac3b71f00298f
nualagr <nualagreenwood@gmail.com>
2021-03-31 10:37:47 +0000

    feat: Add basic Checkout functionality.

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
+from cart.contexts import cart_contents
+
+import stripe
+
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get("cart", {})
     if not cart:
         messages.error(
@@ -12,10 +20,32 @@ def checkout(request):
         )
         return redirect(reverse("products"))
 
+    # Get the contents of the cart from the dict that was added to the request context
+    current_cart = cart_contents(request)
+    total = current_cart["grand_total"]
+    # Convert the grand total from a decimal to an integer for Stripe
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(
+            request,
+            "Stripe public key is missing. \
+            Did you forget to set it in your environment?",
+        )
+
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        "stripe_public_key": stripe_public_key,
+        "client_secret": intent.client_secret,
     }
 
     return render(request, template, context)
---
+++ general_scripts/rename_images.py
@@ -0,0 +1,25 @@
+# Python3 code to rename multiple
+# files in a directory or folder
+
+# importing os module
+import os
+
+
+# Function to rename multiple files
+def main():
+
+    for count, image in enumerate(os.listdir("viridian_images")):
+        new_name = "viridian-" + image
+        source_address = "viridian_images/" + image
+        destination = "viridian_images/" + new_name
+
+        # rename() function will
+        # rename all the files
+        os.rename(source_address, destination)
+
+
+# Driver Code
+if __name__ == '__main__':
+
+    # Calling main() function
+    main()
