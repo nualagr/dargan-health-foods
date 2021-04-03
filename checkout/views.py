db62946c610aeb43ac533a23d3eae1dad3a905c3
nualagr <nualagreenwood@gmail.com>
2021-04-03 16:06:09 +0000

    feat: Add order_history url and view so that Users can access their order history which is now, if they tick the save_info box on their order form, saved to their user profile.

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from cart.contexts import cart_contents
 
 import stripe
@@ -171,21 +173,45 @@ def checkout_success(request, order_number):
     save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
 
-    messages.success(
-        request,
-        f"Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.",
-    )
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the Order
+        order.user_profile = profile
+        order.save()
 
-    # Empty the Shopping Cart
-    if "cart" in request.session:
-        del request.session["cart"]
+        # Save the user's info if the box was checked
+        if save_info:
+            profile_data = {
+                # These dict keys match the fields on the user profile model
+                'default_phone_number': order.phone_number,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_town_or_city': order.town_or_city,
+                'default_county': order.county,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+            }
+            # Create an instance of the User Profile Form using the data
+            # and update the Profile we obtained above
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
-    template = "checkout/checkout_success.html"
-    context = {
-        # Send the order back to the template
-        "order": order,
-    }
+            messages.success(
+                request,
+                f"Order successfully processed! \
+                Your order number is {order_number}. A confirmation \
+                email will be sent to {order.email}.",
+            )
 
-    return render(request, template, context)
+            # Empty the Shopping Cart
+            if "cart" in request.session:
+                del request.session["cart"]
+
+            template = "checkout/checkout_success.html"
+            context = {
+                # Send the order back to the template
+                "order": order,
+            }
+
+            return render(request, template, context)
