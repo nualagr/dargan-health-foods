675e46c5fab5b933f6f3a6994910a29b88fa13e1
nualagr <nualagreenwood@gmail.com>
2021-04-09 10:47:46 +0000

    feat: Create python script to convert csv files with department, category, brand and tag information to formatted json so that the data can be uploaded to the database quickly. Convert the files and save the results in a folder called Fixtures within the Product App.

---
+++ general_scripts/convert_csv_to_nested_json.py
@@ -0,0 +1,53 @@
+# Initial code found at: https://pythonexamples.org/python-csv-to-json/
+# and then modified
+
+import csv
+import json
+
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    """
+    Function to convert CSV to JSON.
+    Takes the file paths as arguments.
+    """
+    # Create an empty list to hold the dicts
+    jsonArray = []
+
+    # Read the file
+    with open(csvFilePath, encoding="utf-8") as csvf:
+        # Load the CSV-file data using CSV library's dictionary reader
+        csvReader = csv.DictReader(csvf)
+
+        # Convert each CSV row into python dict
+        for row in csvReader:
+            # Create a new empty outer dict
+            outRow = {}
+            # Add a new subset dict called fields
+            outRow["fields"] = {}
+            for key, value in row.items():
+                # Add 'name' and 'friendly_name' key value pairs
+                # to the new subset dict
+                if (
+                    key == "name"
+                    or key == "friendly_name"
+                ):
+                    outRow["fields"][key] = value
+                # Add the other key value pairs to the outer dict
+                else:
+                    outRow[key] = value
+
+            # Add this python dict to json array
+            jsonArray.append(outRow)
+
+    # Convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, "w", encoding="utf-8") as jsonf:
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+
+
+# File paths
+csvFilePath = r"./products/fixtures/tags.csv"
+jsonFilePath = r"./products/fixtures/tags.json"
+
+# Call the csv_to_json function
+csv_to_json(csvFilePath, jsonFilePath)
