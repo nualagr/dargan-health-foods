f05d3ada63c65f7e8f9b6b2cb4dd484382d28c99
nualagr <nualagreenwood@gmail.com>
2021-04-30 09:33:22 +0000

    feat: Import the DiscountCode model into the cart/views.py file. This will allow the DiscountCode object to be found in the database and assigned to a variable within view_cart.

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
+from .models import DiscountCode
 from .forms import DiscountCodeForm
 
 
@@ -9,9 +10,45 @@ def view_cart(request):
     A view that renders the shopping cart contents page
     and the empty DiscountCodeForm.
     """
-    cart = request.session.get("cart", {})
-
+    discount = request.session.get("discount", {})
     discount_code_form = DiscountCodeForm()
+
+    if request.method == "POST":
+        discount_code_form = DiscountCodeForm(request.POST)
+        if discount_code_form.is_valid():
+            discount_code = discount_code_form.cleaned_data["discount_code"]
+            print("This is the code that was entered:", discount_code)
+            try:
+                # Get the code from the database
+                discount_code_object = DiscountCode.objects.get(
+                    discount_code=discount_code)
+                print("This is the discount_code_object", discount_code_object)
+                if discount_code_object:
+                    discount["discount_code_id"] = discount_code_object.id
+                    percentage_discount = discount_code_object.percentage_discount
+                    print("This is the percentage discount to be applied:",
+                          percentage_discount)
+                    # Put the promocode variable in the session
+                    request.session["discount"] = discount
+                    return redirect(reverse("view_cart"))
+            except DiscountCode.DoesNotExist:
+                messages.error(
+                        request,
+                        (
+                            "Promo Code not recognised."
+                        ),
+                    )
+                # Return the user to the Shopping Cart page
+                return redirect(reverse("view_cart"))
+        else:
+            discount_code_form = DiscountCodeForm()
+            # Add a message to the request object
+            messages.error(
+                request,
+                "There was an error applying the discount. \
+                Please double check your code.",
+            )
+
     template = "cart/cart.html"
     context = {
         "dcform": discount_code_form,
@@ -124,5 +161,3 @@ def decrease_quantity_by_one(request, item_id):
 
     request.session["cart"] = cart
     return redirect(reverse("view_cart"))
-
-
