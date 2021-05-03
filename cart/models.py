f88bee5b20bc3243da3fc4a8cd2eda754066d224
nualagr <nualagreenwood@gmail.com>
2021-05-03 14:47:36 +0000

    fix: Remove unnecessary decimal places on percentage_discount field.

--- cart/models.py
+++ cart/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class DiscountCode(models.Model):
     discount_code = models.CharField(max_length=30)
-    percentage_discount = models.DecimalField(max_digits=6, decimal_places=2)
+    percentage_discount = models.DecimalField(max_digits=6, decimal_places=0)
 
     def __str__(self):
         return f"PromoCode: {self.discount_code}, {self.percentage_discount}% Discount"
