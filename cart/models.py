cd8720cbf1fee2a94d3d9a0d2548cfd0cd9b5d6d
nualagr <nualagreenwood@gmail.com>
2021-05-05 10:17:47 +0000

    fix: Change the number of decimal palces on the percentage_discount field to 2.

---
+++ cart/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class DiscountCode(models.Model):
+    discount_code = models.CharField(max_length=30)
+    percentage_discount = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"PromoCode: {self.discount_code}, {self.percentage_discount}% Discount"
