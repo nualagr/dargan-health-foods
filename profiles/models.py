59179243d556c59e3ebc4752a795ea77af1b3792
nualagr <nualagreenwood@gmail.com>
2021-05-05 10:15:32 +0000

    feat: Create new DiscountCode2User model. Use foreign key fields to link the model to a User and a Discount Code. Use a Boolean field to indicate whether it is active, i.e. can still be used by the User. Add to the create_or_update_user_profile() function When a new UserProfile is created, automatically create an instance of the DiscountCode2User model for that user and the NEW10, 10% discount code. Using this code, new users can get 10% off their first order.

--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from django_countries.fields import CountryField
+from cart.models import DiscountCode
 
 
 class UserProfile(models.Model):
@@ -40,6 +41,31 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the User Profile
     """
     if created:
-        UserProfile.objects.create(user=instance)
+        new_user = UserProfile.objects.create(user=instance)
+        new_user_discount_code = DiscountCode.objects.get(
+            discount_code="NEW10")
+        DiscountCode2User.objects.create(
+            user=new_user, discount_code=new_user_discount_code)
     # Existing user - so save the profile to update it
     instance.userprofile.save()
+
+
+class DiscountCode2User(models.Model):
+    discount_code = models.ForeignKey(
+        DiscountCode,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    active = models.BooleanField(
+        default=True,
+    )
+
+    def __str__(self):
+        return f"{self.user}'s PromoCode: {self.discount_code}, Valid: {self.active}"
