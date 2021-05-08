946a0bd4ffe438cf14770de3b951bddd5868ca5a
nualagr <nualagreenwood@gmail.com>
2021-05-08 14:25:04 +0000

    feat: Set SECRET_KEY and DEBUG variables.
    In settings.py, check environ for the SECRET_KEY variable or set to a blank string.
    Set DEBUG to be True only if there's a variable called 'development' in the environment.
    This ensures that the SECRET_KEY will not be visible in version control.
    This also ensures that DEBUG will be False on Heroku.

--- dargan_health_foods/settings.py
+++ dargan_health_foods/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "3o$e8&sp+(pv0#&#vc@@r-5!@7m$no9)&@%f*ucr=g5w&j-xog"
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["dargan-health-foods.herokuapp.com", "localhost"]
 
