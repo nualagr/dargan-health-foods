f45731388270d6475731ff11e292f8d67fdb084c
nualagr <nualagreenwood@gmail.com>
2021-05-18 10:24:14 +0000

    fix: Add media query to deal with extra small mobile screens.
    Reduce the font size of text, inputs and buttons
    when viewed on extra small mobile screens,
    those 280px and smaller, such as the Galaxy Fold.

--- cart/contexts.py
+++ cart/contexts.py
@@ -26,11 +26,13 @@ def cart_contents(request):
         else:
             total += quantity * product.price
         product_count += quantity
-        cart_items.append({
-            "item_id": item_id,
-            "quantity": quantity,
-            "product": product,
-        })
+        cart_items.append(
+            {
+                "item_id": item_id,
+                "quantity": quantity,
+                "product": product,
+            }
+        )
     # If the user is logged in, then check discount
     if request.user.is_authenticated:
         user = UserProfile.objects.get(user=request.user)
@@ -39,15 +41,20 @@ def cart_contents(request):
         if discount:
             # Check to see whether the DiscountCode2User for this user exists
             discount_code_object = DiscountCode.objects.filter(
-                pk=discount["discount_code_id"]).first()
+                pk=discount["discount_code_id"]
+            ).first()
             user_discount_code = DiscountCode2User.objects.filter(
-                discount_code=discount_code_object,
-                user=user).first()
+                discount_code=discount_code_object, user=user
+            ).first()
 
             # Check if the discount exists
             # if not delete it from the user's session
             # (Condensing of 'if' statements suggested by Mr. Reuben Ferrante)
-            if discount_code_object and user_discount_code and user_discount_code.active:
+            if (
+                discount_code_object
+                and user_discount_code
+                and user_discount_code.active
+            ):
                 total_before_discount = total
                 # Calculate the discount to be applied
                 percentage_discount = discount_code_object.percentage_discount
